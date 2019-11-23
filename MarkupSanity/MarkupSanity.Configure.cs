f4c64ddca48e8fd071819d45363561133c7b5ac3
Rafael Silveira Cordeiro <rafaelsc@rafaelsc.net>
2019-11-22 19:08:08 -0800

    Fix a Bug when whitelistedTags, whitelistedAttributes, scriptableAttributes are empty

--- MarkupSanity/MarkupSanity.Configure.cs
+++ MarkupSanity/MarkupSanity.Configure.cs
@@ -15,23 +15,22 @@ namespace RockFluid
             /// <summary>
             /// Gets or sets the custom whitelist of allowed tags to validate against instead of the built-in ones.
             /// </summary>
-            public List<String> CustomWhitelistedTags { get; set; } = new List<String>();
+            public List<String> CustomWhitelistedTags { get; set; }
 
             /// <summary>
             /// Gets or sets the custom whitelist of allowed attributes to validate against instead of the built-in ones.
             /// </summary>
-            public List<String> CustomWhitelistedAttributes { get; set; } = new List<String>();
+            public List<String> CustomWhitelistedAttributes { get; set; }
 
             /// <summary>
             /// Gets or sets the custom list of known attributes that supports and executes javascript code. These attributes shall be inspected to ensure they don't contain any code that may potentially become dangerous.
             /// </summary>
-            public List<String> CustomScriptableAttributes { get; set; } = new List<String>();
+            public List<String> CustomScriptableAttributes { get; set; }
 
             /// <summary>
             /// Gets or sets the custom list of known attribute signatures that supports and executes javascript code. If these signatures are found in attribute values, the whole attribute shall be removed.
             /// </summary>
-            public List<String> CustomScriptableAttributeSignatures = new List<String>();
-
+            public List<String> CustomScriptableAttributeSignatures { get; set; }
 
             /// <summary>
             /// Gets or sets the custom list of blacklisted tags. These tags are always removed from internal list and custom whitelists.
@@ -39,8 +38,7 @@ namespace RockFluid
             /// <remarks>
             /// This removes tags from internal and custom whitelists, for cases when internal list is acceptable except for a few tags configured in it.
             /// </remarks>
-            public List<String> CustomBlacklistedTags { get; set; } = new List<String>();
-
+            public List<String> CustomBlacklistedTags { get; set; }
 
             /// <summary>
             /// Gets or sets the custom list of special supplemental tags to extend the default WhitelistedTags list. These are not added to the CustomWhitelistedTags property.
@@ -48,7 +46,7 @@ namespace RockFluid
             /// <remarks>
             /// Add values here if the defaults are acceptable, but would like to add more to it.
             /// </remarks>
-            public List<String> SupplementalTags { get; set; } = new List<String>();
+            public List<String> SupplementalTags { get; set; }
 
             /// <summary>
             /// Gets or sets the custom list of special supplemental attributes to extend the default WhitelistedAttributes list. These are not added to the CustomWhitelistedAttributes property.
@@ -56,7 +54,7 @@ namespace RockFluid
             /// <remarks>
             /// Add values here if the defaults are acceptable, but would like to add more to it.
             /// </remarks>
-            public List<String> SupplementalAttributes { get; set; } = new List<String>();
+            public List<String> SupplementalAttributes { get; set; }
 
             /// <summary>
             /// Gets or sets the custom list of special supplemental scriptable attributes to extend the default ScriptableAttributes list. These are not added to the CustomScriptableAttributes property.
@@ -64,7 +62,7 @@ namespace RockFluid
             /// <remarks>
             /// Add values here if the defaults are acceptable, but would like to add more to it.
             /// </remarks>
-            public List<String> SupplementalScriptableAttributes { get; set; } = new List<String>();
+            public List<String> SupplementalScriptableAttributes { get; set; }
 
             /// <summary>
             /// Gets or sets whether to remove the entire node (tag and contents) or only the invalid tag (retaining content).
